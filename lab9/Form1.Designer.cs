e223eaa8ae92b49bc5f040cf3b8b11841042207a
Raiden02s <darilevskijoleksej@gmail.com>
2025-06-06 13:21:05 +0300

    lab9

---
+++ lab9/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ParametricCurvePlotter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освобождение ресурсов.
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный дизайнером Windows Form
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.buttonPlot = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 57);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(700, 462);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(12, 12);
+            this.textBoxA.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(58, 23);
+            this.textBoxA.TabIndex = 1;
+            this.textBoxA.Text = "1.0";
+            this.textBoxA.TextChanged += new System.EventHandler(this.textBoxA_TextChanged);
+            // 
+            // buttonPlot
+            // 
+            this.buttonPlot.Location = new System.Drawing.Point(82, 12);
+            this.buttonPlot.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonPlot.Name = "buttonPlot";
+            this.buttonPlot.Size = new System.Drawing.Size(88, 27);
+            this.buttonPlot.TabIndex = 2;
+            this.buttonPlot.Text = "Plot";
+            this.buttonPlot.UseVisualStyleBackColor = true;
+            this.buttonPlot.Click += new System.EventHandler(this.buttonPlot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 531);
+            this.Controls.Add(this.buttonPlot);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Parametric Curve Plotter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button buttonPlot;
+    }
+}
---
+++ lab9/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ParametricCurvePlotter
+{
+    public partial class Form1 : Form
+    {
+        private float a = 1.0f; // Default value for coefficient a
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPlot_Click(object sender, EventArgs e)
+        {
+            if (float.TryParse(textBoxA.Text, out float value))
+            {
+                a = value;
+                pictureBox1.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number for 'a'.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            float scale = 50.0f; // Scaling factor for graph
+            float centerX = width / 2.0f;
+            float centerY = height / 2.0f;
+
+            // Draw axes
+            g.DrawLine(Pens.Black, 0, (int)centerY, width, (int)centerY); // X-axis
+            g.DrawLine(Pens.Black, (int)centerX, 0, (int)centerX, height); // Y-axis
+
+            // Label axes
+            g.DrawString("X", new Font("Arial", 10), Brushes.Black, width - 15, centerY - 5);
+            g.DrawString("Y", new Font("Arial", 10), Brushes.Black, centerX + 5, 5);
+
+            // Add tick marks and labels (e.g., -5 to 5)
+            for (int i = -5; i <= 5; i++)
+            {
+                float x = centerX + i * scale;
+                float y = centerY - i * scale;
+                g.DrawLine(Pens.Black, x, centerY - 2, x, centerY + 2); // X-axis ticks
+                g.DrawLine(Pens.Black, centerX - 2, y, centerX + 2, y); // Y-axis ticks
+                g.DrawString(i.ToString(), new Font("Arial", 8), Brushes.Black, x - 5, centerY + 5);
+                g.DrawString(i.ToString(), new Font("Arial", 8), Brushes.Black, centerX + 5, y - 5);
+            }
+
+            // Plot parametric curve
+            PointF[] points = new PointF[1000];
+            int pointIndex = 0;
+
+            for (float t = -5.0f; t <= 5.0f; t += 0.01f)
+            {
+                if (Math.Abs(1 + t * t * t) < 0.001f) continue; // Avoid division by zero near t = -1
+                float x = (3 * a * t) / (1 + t * t * t);
+                float y = (3 * a * t * t) / (1 + t * t * t);
+                points[pointIndex] = new PointF(centerX + x * scale, centerY - y * scale);
+                pointIndex++;
+            }
+
+            // Create a new array with the exact number of points to draw
+            PointF[] pointsToDraw = new PointF[pointIndex];
+            Array.Copy(points, 0, pointsToDraw, 0, pointIndex);
+
+            // Draw the curve
+            if (pointsToDraw.Length > 1) // Ensure there are at least 2 points to draw a line
+            {
+                g.DrawLines(Pens.Red, pointsToDraw);
+            }
+        }
+
+        private void textBoxA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
