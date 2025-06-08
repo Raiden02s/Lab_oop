2ef9f93681aefd23f19cd0d48ba8c56c723391a8
Raiden02s <darilevskijoleksej@gmail.com>
2025-06-08 18:47:07 +0300

    lab18

---
+++ lab18/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ProcessManagerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBoxProcesses = new System.Windows.Forms.ListBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnShowThreadsModules = new System.Windows.Forms.Button(); // New button
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showDetailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.killProcessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBoxProcesses
+            // 
+            this.listBoxProcesses.FormattingEnabled = true;
+            this.listBoxProcesses.Location = new System.Drawing.Point(12, 12);
+            this.listBoxProcesses.Name = "listBoxProcesses";
+            this.listBoxProcesses.Size = new System.Drawing.Size(300, 355);
+            this.listBoxProcesses.TabIndex = 0;
+            this.listBoxProcesses.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBoxProcesses_MouseDown);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(318, 12);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(318, 41);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnShowThreadsModules
+            // 
+            this.btnShowThreadsModules.Location = new System.Drawing.Point(318, 70);
+            this.btnShowThreadsModules.Name = "btnShowThreadsModules";
+            this.btnShowThreadsModules.Size = new System.Drawing.Size(75, 23);
+            this.btnShowThreadsModules.TabIndex = 3;
+            this.btnShowThreadsModules.Text = "Show Threads & Modules";
+            this.btnShowThreadsModules.UseVisualStyleBackColor = true;
+            this.btnShowThreadsModules.Click += new System.EventHandler(this.btnShowThreadsModules_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showDetailsToolStripMenuItem,
+            this.killProcessToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 48);
+            // 
+            // showDetailsToolStripMenuItem
+            // 
+            this.showDetailsToolStripMenuItem.Name = "showDetailsToolStripMenuItem";
+            this.showDetailsToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.showDetailsToolStripMenuItem.Text = "Show Details";
+            this.showDetailsToolStripMenuItem.Click += new System.EventHandler(this.showDetailsToolStripMenuItem_Click);
+            // 
+            // killProcessToolStripMenuItem
+            // 
+            this.killProcessToolStripMenuItem.Name = "killProcessToolStripMenuItem";
+            this.killProcessToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.killProcessToolStripMenuItem.Text = "Kill Process";
+            this.killProcessToolStripMenuItem.Click += new System.EventHandler(this.killProcessToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 379);
+            this.Controls.Add(this.btnShowThreadsModules);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.listBoxProcesses);
+            this.Name = "Form1";
+            this.Text = "Process Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxProcesses;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnShowThreadsModules; // New field
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showDetailsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem killProcessToolStripMenuItem;
+    }
+}
---
+++ lab18/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ProcessManagerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listBoxProcesses.ContextMenuStrip = contextMenuStrip1; // Attach context menu to ListBox
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadProcesses();
+        }
+
+        private void LoadProcesses()
+        {
+            listBoxProcesses.Items.Clear();
+            foreach (Process process in Process.GetProcesses())
+            {
+                listBoxProcesses.Items.Add($"{process.Id} - {process.ProcessName}");
+            }
+        }
+
+        private void listBoxProcesses_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int index = listBoxProcesses.IndexFromPoint(e.Location);
+                if (index != ListBox.NoMatches)
+                {
+                    listBoxProcesses.SelectedIndex = index;
+                    contextMenuStrip1.Show(listBoxProcesses, e.Location);
+                }
+            }
+        }
+
+        private void showDetailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex != -1)
+            {
+                string selectedItem = listBoxProcesses.SelectedItem.ToString();
+                int processId = int.Parse(selectedItem.Split('-')[0].Trim());
+                try
+                {
+                    Process process = Process.GetProcessById(processId);
+
+                    string details = $"Process Details:\n" +
+                                    $"ID: {process.Id}\n" +
+                                    $"Name: {process.ProcessName}\n" +
+                                    $"Start Time: {process.StartTime}\n";
+                    // Handle virtual memory size with error checking
+                    try
+                    {
+                        details += $"Virtual Memory: {process.VirtualMemorySize64 / (1024 * 1024)} MB\n";
+                    }
+                    catch (Exception ex)
+                    {
+                        details += $"Virtual Memory: Error - {ex.Message} (Run as Administrator may be required)\n";
+                    }
+
+                    MessageBox.Show(details, "Process Details");
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("The process has terminated or is no longer available.", "Error");
+                }
+                catch (Win32Exception ex)
+                {
+                    MessageBox.Show($"Unable to access process details: {ex.Message} (Run as Administrator may be required)", "Error");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Unexpected error: {ex.Message}", "Error");
+                }
+            }
+        }
+
+        private void killProcessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex != -1)
+            {
+                string selectedItem = listBoxProcesses.SelectedItem.ToString();
+                int processId = int.Parse(selectedItem.Split('-')[0].Trim());
+                if (MessageBox.Show($"Are you sure you want to kill process {processId}?", "Confirm Kill", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        Process process = Process.GetProcessById(processId);
+                        process.Kill();
+                        LoadProcesses(); // Refresh the list after killing
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("The process has terminated or is no longer available.", "Error");
+                    }
+                    catch (Win32Exception ex)
+                    {
+                        MessageBox.Show($"Unable to kill process: {ex.Message} (Run as Administrator may be required)", "Error");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Unexpected error killing process: {ex.Message}", "Error");
+                    }
+                }
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadProcesses();
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                sfd.Title = "Export Process List";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        using (StreamWriter sw = new StreamWriter(sfd.FileName))
+                        {
+                            foreach (var item in listBoxProcesses.Items)
+                            {
+                                sw.WriteLine(item.ToString());
+                            }
+                            MessageBox.Show("Process list exported successfully!");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error exporting file: {ex.Message}", "Error");
+                    }
+                }
+            }
+        }
+
+        private void btnShowThreadsModules_Click(object sender, EventArgs e)
+        {
+            if (listBoxProcesses.SelectedIndex != -1)
+            {
+                string selectedItem = listBoxProcesses.SelectedItem.ToString();
+                int processId = int.Parse(selectedItem.Split('-')[0].Trim());
+                try
+                {
+                    Process process = Process.GetProcessById(processId);
+                    ProcessDetailsForm detailsForm = new ProcessDetailsForm(process);
+                    detailsForm.ShowDialog();
+                }
+                catch (ArgumentException)
+                {
+                    MessageBox.Show("The process has terminated or is no longer available.", "Error");
+                }
+                catch (Win32Exception ex)
+                {
+                    MessageBox.Show($"Unable to access process: {ex.Message} (Run as Administrator may be required)", "Error");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Unexpected error: {ex.Message}", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a process first.");
+            }
+        }
+    }
+
+    // Updated form to display thread and module details with enhanced error handling
+    public partial class ProcessDetailsForm : Form
+    {
+        private Process process;
+        private TextBox txtDetails;
+
+        public ProcessDetailsForm(Process process)
+        {
+            this.process = process ?? throw new ArgumentNullException(nameof(process)); // Ensure process is not null
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Text = "Threads and Modules";
+            this.Size = new System.Drawing.Size(400, 300);
+
+            txtDetails = new TextBox
+            {
+                Multiline = true,
+                ScrollBars = ScrollBars.Vertical,
+                Dock = DockStyle.Fill,
+                ReadOnly = true,
+                Font = new System.Drawing.Font("Consolas", 10)
+            };
+
+            this.Controls.Add(txtDetails);
+            LoadDetails();
+        }
+
+        private void LoadDetails()
+        {
+            txtDetails.Clear();
+            txtDetails.AppendText($"Process: {process.ProcessName} (ID: {process.Id})\n\n");
+
+            txtDetails.AppendText("Threads:\n");
+            try
+            {
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    txtDetails.AppendText($"  ID: {thread.Id}, Priority: {thread.PriorityLevel}, Start Time: {thread.StartTime}\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                txtDetails.AppendText($"  Error accessing threads: {ex.Message}\n");
+            }
+
+            txtDetails.AppendText("\nModules:\n");
+            try
+            {
+                foreach (ProcessModule module in process.Modules)
+                {
+                    txtDetails.AppendText($"  Name: {module.ModuleName}, File: {module.FileName}, Memory Size: {module.ModuleMemorySize} bytes\n");
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                txtDetails.AppendText($"  Error accessing modules: {ex.Message} (Run as Administrator may be required)\n");
+            }
+            catch (Exception ex)
+            {
+                txtDetails.AppendText($"  Unexpected error accessing modules: {ex.Message}\n");
+            }
+        }
+    }
+}
