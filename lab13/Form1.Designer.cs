6f9242c06109213fa4e22ab1968b603921e8d424
Raiden02s <darilevskijoleksej@gmail.com>
2025-06-11 01:25:43 +0300

    lab13

---
+++ lab13/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace FileSystemExplorer
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.treeViewDrives = new System.Windows.Forms.TreeView();
+            this.listViewItems = new System.Windows.Forms.ListView();
+            this.columnHeaderName = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderType = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderDate = new System.Windows.Forms.ColumnHeader();
+            this.columnHeaderSize = new System.Windows.Forms.ColumnHeader();
+            this.textBoxProperties = new System.Windows.Forms.TextBox();
+            this.labelProperties = new System.Windows.Forms.Label();
+            this.textBoxDirFilter = new System.Windows.Forms.TextBox();
+            this.labelDirFilter = new System.Windows.Forms.Label();
+            this.textBoxFileFilter = new System.Windows.Forms.TextBox();
+            this.labelFileFilter = new System.Windows.Forms.Label();
+            this.pictureBoxPreview = new System.Windows.Forms.PictureBox();
+            this.textBoxContent = new System.Windows.Forms.TextBox();
+            this.labelPreview = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).BeginInit();
+            this.SuspendLayout();
+
+            // treeViewDrives
+            this.treeViewDrives.Location = new System.Drawing.Point(12, 12);
+            this.treeViewDrives.Size = new System.Drawing.Size(200, 600);
+            this.treeViewDrives.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeViewDrives_BeforeExpand);
+            this.treeViewDrives.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewDrives_AfterSelect);
+
+            // listViewItems
+            this.listViewItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+                this.columnHeaderName,
+                this.columnHeaderType,
+                this.columnHeaderDate,
+                this.columnHeaderSize});
+            this.listViewItems.Location = new System.Drawing.Point(218, 40);
+            this.listViewItems.Size = new System.Drawing.Size(550, 300);
+            this.listViewItems.View = System.Windows.Forms.View.Details;
+            this.listViewItems.SelectedIndexChanged += new System.EventHandler(this.listViewItems_SelectedIndexChanged);
+
+            // columnHeaderName
+            this.columnHeaderName.Text = "Name";
+            this.columnHeaderName.Width = 200;
+
+            // columnHeaderType
+            this.columnHeaderType.Text = "Type";
+            this.columnHeaderType.Width = 100;
+
+            // columnHeaderDate
+            this.columnHeaderDate.Text = "Date Modified";
+            this.columnHeaderDate.Width = 150;
+
+            // columnHeaderSize
+            this.columnHeaderSize.Text = "Size";
+            this.columnHeaderSize.Width = 100;
+
+            // textBoxProperties
+            this.textBoxProperties.Location = new System.Drawing.Point(218, 370);
+            this.textBoxProperties.Multiline = true;
+            this.textBoxProperties.Size = new System.Drawing.Size(550, 200);
+            this.textBoxProperties.ReadOnly = true;
+
+            // labelProperties
+            this.labelProperties.Location = new System.Drawing.Point(218, 346);
+            this.labelProperties.Size = new System.Drawing.Size(100, 23);
+            this.labelProperties.Text = "Properties";
+
+            // textBoxDirFilter
+            this.textBoxDirFilter.Location = new System.Drawing.Point(300, 12);
+            this.textBoxDirFilter.Size = new System.Drawing.Size(150, 23);
+            this.textBoxDirFilter.TextChanged += new System.EventHandler(this.textBoxDirFilter_TextChanged);
+
+            // labelDirFilter
+            this.labelDirFilter.Location = new System.Drawing.Point(218, 12);
+            this.labelDirFilter.Size = new System.Drawing.Size(80, 23);
+            this.labelDirFilter.Text = "Directory Filter";
+
+            // textBoxFileFilter
+            this.textBoxFileFilter.Location = new System.Drawing.Point(610, 12);
+            this.textBoxFileFilter.Size = new System.Drawing.Size(150, 23);
+            this.textBoxFileFilter.TextChanged += new System.EventHandler(this.textBoxFileFilter_TextChanged);
+
+            // labelFileFilter
+            this.labelFileFilter.Location = new System.Drawing.Point(528, 12);
+            this.labelFileFilter.Size = new System.Drawing.Size(80, 23);
+            this.labelFileFilter.Text = "File Filter";
+
+            // pictureBoxPreview
+            this.pictureBoxPreview.Location = new System.Drawing.Point(774, 40);
+            this.pictureBoxPreview.Size = new System.Drawing.Size(300, 300);
+            this.pictureBoxPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+
+            // textBoxContent
+            this.textBoxContent.Location = new System.Drawing.Point(774, 370);
+            this.textBoxContent.Multiline = true;
+            this.textBoxContent.Size = new System.Drawing.Size(300, 200);
+            this.textBoxContent.ReadOnly = true;
+
+            // labelPreview
+            this.labelPreview.Location = new System.Drawing.Point(774, 346);
+            this.labelPreview.Size = new System.Drawing.Size(100, 23);
+            this.labelPreview.Text = "Preview";
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 624);
+            this.Controls.Add(this.treeViewDrives);
+            this.Controls.Add(this.listViewItems);
+            this.Controls.Add(this.textBoxProperties);
+            this.Controls.Add(this.labelProperties);
+            this.Controls.Add(this.textBoxDirFilter);
+            this.Controls.Add(this.labelDirFilter);
+            this.Controls.Add(this.textBoxFileFilter);
+            this.Controls.Add(this.labelFileFilter);
+            this.Controls.Add(this.pictureBoxPreview);
+            this.Controls.Add(this.textBoxContent);
+            this.Controls.Add(this.labelPreview);
+            this.Text = "File System Explorer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.TreeView treeViewDrives;
+        private System.Windows.Forms.ListView listViewItems;
+        private System.Windows.Forms.ColumnHeader columnHeaderName;
+        private System.Windows.Forms.ColumnHeader columnHeaderType;
+        private System.Windows.Forms.ColumnHeader columnHeaderDate;
+        private System.Windows.Forms.ColumnHeader columnHeaderSize;
+        private System.Windows.Forms.TextBox textBoxProperties;
+        private System.Windows.Forms.Label labelProperties;
+        private System.Windows.Forms.TextBox textBoxDirFilter;
+        private System.Windows.Forms.Label labelDirFilter;
+        private System.Windows.Forms.TextBox textBoxFileFilter;
+        private System.Windows.Forms.Label labelFileFilter;
+        private System.Windows.Forms.PictureBox pictureBoxPreview;
+        private System.Windows.Forms.TextBox textBoxContent;
+        private System.Windows.Forms.Label labelPreview;
+    }
+}
---
+++ lab13/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Security.AccessControl;
+using System.Drawing;
+using System.Linq;
+
+namespace FileSystemExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBoxFileFilter.Text = ""; // Clear file filter on startup
+            textBoxDirFilter.Text = ""; // Clear directory filter on startup
+            LoadDrives();
+        }
+
+        private void LoadDrives()
+        {
+            treeViewDrives.Nodes.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady)
+                {
+                    TreeNode node = new TreeNode(drive.Name);
+                    node.Tag = drive;
+                    node.Nodes.Add("Loading...");
+                    treeViewDrives.Nodes.Add(node);
+                }
+            }
+        }
+
+        private void treeViewDrives_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode node = e.Node;
+            node.Nodes.Clear();
+            try
+            {
+                DirectoryInfo dirInfo = node.Tag is DriveInfo ? ((DriveInfo)node.Tag).RootDirectory : new DirectoryInfo((string)node.Tag);
+                foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+                {
+                    if ((dir.Attributes & FileAttributes.Hidden) == 0)
+                    {
+                        TreeNode subNode = new TreeNode(dir.Name);
+                        subNode.Tag = dir.FullName;
+                        subNode.Nodes.Add("Loading...");
+                        node.Nodes.Add(subNode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error accessing directory: {ex.Message}");
+            }
+        }
+
+        private void treeViewDrives_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            listViewItems.Items.Clear();
+            textBoxProperties.Clear();
+            pictureBoxPreview.Image = null;
+            textBoxContent.Clear();
+
+            try
+            {
+                string path = e.Node.Tag is DriveInfo ? ((DriveInfo)e.Node.Tag).RootDirectory.FullName : (string)e.Node.Tag;
+                DirectoryInfo dirInfo = new DirectoryInfo(path);
+                if (dirInfo.Exists)
+                {
+                    DisplayDirectoryProperties(dirInfo);
+                    PopulateListView(dirInfo);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error accessing directory: {ex.Message}");
+            }
+        }
+
+        private void PopulateListView(DirectoryInfo dirInfo)
+        {
+            listViewItems.Items.Clear();
+            string dirFilter = textBoxDirFilter.Text.Trim().ToLower();
+            string fileFilter = textBoxFileFilter.Text.Trim().ToLower();
+
+            try
+            {
+                // Populate directories
+                foreach (DirectoryInfo dir in dirInfo.GetDirectories())
+                {
+                    if ((dir.Attributes & FileAttributes.Hidden) == 0 &&
+                        (string.IsNullOrEmpty(dirFilter) || dir.Name.ToLower().Contains(dirFilter)))
+                    {
+                        ListViewItem item = new ListViewItem(dir.Name);
+                        item.SubItems.Add("Directory");
+                        item.SubItems.Add(dir.LastWriteTime.ToString());
+                        item.Tag = dir;
+                        listViewItems.Items.Add(item);
+                    }
+                }
+
+                // Populate files
+                foreach (FileInfo file in dirInfo.GetFiles())
+                {
+                    if ((file.Attributes & FileAttributes.Hidden) == 0 &&
+                        (string.IsNullOrEmpty(fileFilter) || file.Name.ToLower().Contains(fileFilter)))
+                    {
+                        // Debug: Log file to console to verify enumeration
+                        Console.WriteLine($"Found file: {file.Name}");
+                        ListViewItem item = new ListViewItem(file.Name);
+                        item.SubItems.Add(file.Extension);
+                        item.SubItems.Add(file.LastWriteTime.ToString());
+                        item.SubItems.Add((file.Length / 1024.0).ToString("N2") + " KB");
+                        item.Tag = file;
+                        listViewItems.Items.Add(item);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Access denied to some items: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error listing items: {ex.Message}");
+            }
+        }
+
+        private void DisplayDirectoryProperties(DirectoryInfo dirInfo)
+        {
+            textBoxProperties.Text = $"Directory Name: {dirInfo.Name}\r\n" +
+                                    $"Full Path: {dirInfo.FullName}\r\n" +
+                                    $"Creation Time: {dirInfo.CreationTime}\r\n" +
+                                    $"Last Access: {dirInfo.LastAccessTime}\r\n" +
+                                    $"Last Write: {dirInfo.LastWriteTime}\r\n" +
+                                    $"Root: {dirInfo.Root}\r\n";
+
+            try
+            {
+                DirectorySecurity security = dirInfo.GetAccessControl();
+                foreach (FileSystemAccessRule rule in security.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount)))
+                {
+                    textBoxProperties.Text += $"Access Rule: {rule.IdentityReference} - {rule.FileSystemRights} - {rule.AccessControlType}\r\n";
+                }
+            }
+            catch (Exception ex)
+            {
+                textBoxProperties.Text += $"Security Info Error: {ex.Message}\r\n";
+            }
+        }
+
+        private void listViewItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listViewItems.SelectedItems.Count > 0)
+            {
+                var item = listViewItems.SelectedItems[0];
+                textBoxProperties.Clear();
+                pictureBoxPreview.Image = null;
+                textBoxContent.Clear();
+
+                if (item.Tag is DirectoryInfo dirInfo)
+                {
+                    DisplayDirectoryProperties(dirInfo);
+                }
+                else if (item.Tag is FileInfo fileInfo)
+                {
+                    textBoxProperties.Text = $"File Name: {fileInfo.Name}\r\n" +
+                                            $"Full Path: {fileInfo.FullName}\r\n" +
+                                            $"Creation Time: {fileInfo.CreationTime}\r\n" +
+                                            $"Last Access: {fileInfo.LastAccessTime}\r\n" +
+                                            $"Last Write: {fileInfo.LastWriteTime}\r\n" +
+                                            $"Size: {(fileInfo.Length / 1024.0).ToString("N2")} KB\r\n" +
+                                            $"Extension: {fileInfo.Extension}\r\n";
+
+                    try
+                    {
+                        FileSecurity security = fileInfo.GetAccessControl();
+                        foreach (FileSystemAccessRule rule in security.GetAccessRules(true, true, typeof(System.Security.Principal.NTAccount)))
+                        {
+                            textBoxProperties.Text += $"Access Rule: {rule.IdentityReference} - {rule.FileSystemRights} - {rule.AccessControlType}\r\n";
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        textBoxProperties.Text += $"Security Info Error: {ex.Message}\r\n";
+                    }
+
+                    if (fileInfo.Extension.ToLower().Contains("txt"))
+                    {
+                        try
+                        {
+                            textBoxContent.Text = File.ReadAllText(fileInfo.FullName);
+                        }
+                        catch (Exception ex)
+                        {
+                            textBoxContent.Text = $"Error reading text file: {ex.Message}";
+                        }
+                    }
+                    else if (new[] { ".jpg", ".jpeg", ".png", ".bmp" }.Contains(fileInfo.Extension.ToLower()))
+                    {
+                        try
+                        {
+                            pictureBoxPreview.Image = Image.FromFile(fileInfo.FullName);
+                        }
+                        catch (Exception ex)
+                        {
+                            pictureBoxPreview.Image = null;
+                            textBoxContent.Text = $"Error loading image: {ex.Message}";
+                        }
+                    }
+                }
+            }
+        }
+
+        private void textBoxDirFilter_TextChanged(object sender, EventArgs e)
+        {
+            if (treeViewDrives.SelectedNode != null)
+            {
+                string path = treeViewDrives.SelectedNode.Tag is DriveInfo ? ((DriveInfo)treeViewDrives.SelectedNode.Tag).RootDirectory.FullName : (string)treeViewDrives.SelectedNode.Tag;
+                PopulateListView(new DirectoryInfo(path));
+            }
+        }
+
+        private void textBoxFileFilter_TextChanged(object sender, EventArgs e)
+        {
+            if (treeViewDrives.SelectedNode != null)
+            {
+                string path = treeViewDrives.SelectedNode.Tag is DriveInfo ? ((DriveInfo)treeViewDrives.SelectedNode.Tag).RootDirectory.FullName : (string)treeViewDrives.SelectedNode.Tag;
+                PopulateListView(new DirectoryInfo(path));
+            }
+        }
+    }
+}
